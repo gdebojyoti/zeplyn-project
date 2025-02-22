41f15d7c9644c31ce8626fb27641e4a3b2ddbfee
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2025-02-23 03:10:12 +0530

    Suppressed ts errors for vercel deployment.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+
+// @ts-ignore
 import App from './App'
 
+// @ts-ignore
 import store from './store'
 
 createRoot(document.getElementById('root')!).render(
